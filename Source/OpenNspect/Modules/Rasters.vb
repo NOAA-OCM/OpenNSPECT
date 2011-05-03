7b4e8db7e8f3effd2a1a81a6280cf7e8fa835f0b
mudnug_cp <SND\mudnug_cp>
2011-05-03 20:37:17 +0000

    Allow esri grids and other types

--- Source/OpenNspect/Modules/Rasters.vb
+++ Source/OpenNspect/Modules/Rasters.vb
@@ -42,21 +42,15 @@ Module Rasters
         Return tmpraster
     End Function
 
-    Public Function AddRasterLayerToMapFromFileName(ByRef directory As String) As Boolean
-        MapWindowPlugin.MapWindowInstance.Layers.Add(directory + "\sta.adf")
-        Return True
-        ' todo: was this for backwards compatibility?
-        'Try
-        '    If Path.GetExtension(directory) <> "" Then ' if this is a file
-        '        MapWindowPlugin.MapWindowInstance.Layers.Add(directory)
-        '    Else ' we were passed a directory
-        '        MapWindowPlugin.MapWindowInstance.Layers.Add(directory + "\sta.adf")
-        '    End If
-        '    Return True
-        'Catch ex As Exception
-        '    Return False
-        'End Try
+    Public Function AddRasterLayerToMapFromFileName(ByRef fileOrDirectory As String) As Boolean
+        If Not Path.HasExtension(fileOrDirectory) Then
+            ' we were passed a directory which represents an Esri Grid.
+            fileOrDirectory = fileOrDirectory + "\sta.adf"
+        End If
 
+        MapWindowPlugin.MapWindowInstance.Layers.Add(fileOrDirectory)
+
+        Return True
     End Function
 
     Public Function ReturnRasterStretchColorRampCS(ByRef pRaster As Grid, ByRef strColor As String) As GridColorScheme
