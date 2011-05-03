06c602f183ccb266f5a2f82575a541ac87833749
mudnug_cp <SND\mudnug_cp>
2011-05-03 23:45:15 +0000

    Reduced method by 70 lines.

--- Source/OpenNspect/OpenNspect.MapWindow4Plugin/Modules/Rasters.vb
+++ Source/OpenNspect/OpenNspect.MapWindow4Plugin/Modules/Rasters.vb
@@ -53,181 +53,100 @@ Module Rasters
         Return True
     End Function
 
-    Public Function ReturnRasterStretchColorRampCS(ByRef pRaster As Grid, ByRef strColor As String) As GridColorScheme
-        ReturnRasterStretchColorRampCS = Nothing
+    Public Function GetRasterStretchColorRampCS(ByRef pRaster As Grid, ByRef strColor As String) As GridColorScheme
+        ' The commented values look like other colors that could be substituted.
         Try
-            Dim rTo As Integer
-            Dim bTo As Integer
-            Dim gTo As Integer
-            Dim rFrom As Integer
-            Dim bFrom As Integer
-            Dim gFrom As Integer
-            Dim rTo2 As Integer
-            Dim bTo2 As Integer
-            Dim gTo2 As Integer
-            Dim rFrom2 As Integer
-            Dim bFrom2 As Integer
-            Dim gFrom2 As Integer
-            'System.Drawing.Color.Brown.R
+            Dim fromColor As Integer
+            Dim toColor As Integer
+            Dim fromColor2 As Integer
+            Dim toColor2 As Integer
+
             Select Case strColor
                 Case "Blue"
-                    ' 242, 245, 255
-                    rFrom = 242
-                    gFrom = 245
-                    bFrom = 255
-
+                    fromColor = RGB(242, 245, 255)
+                    toColor = RGB(91, 129, 255)
                     '149, 173, 255
-                    rTo = 91
-                    gTo = 129
-                    bTo = 255
 
-                    '60, 103, 255
-                    rFrom2 = 60
-                    gFrom2 = 103
-                    bFrom2 = 255
+                    fromColor2 = RGB(60, 103, 255)
+                    toColor2 = RGB(18, 73, 255)
 
-                    '18, 73, 255
-                    rTo2 = 18
-                    gTo2 = 73
-                    bTo2 = 255
                 Case "Brown"
-                    '255, 242, 217
-                    rFrom = 255
-                    gFrom = 242
-                    bFrom = 217
+                    fromColor = RGB(255, 242, 217)
+                    toColor = RGB(255, 202, 102)
 
-                    '255, 202, 102
-                    rTo = 255
-                    gTo = 202
-                    bTo = 102
+                    fromColor2 = RGB(255, 174, 23)
+                    toColor2 = RGB(255, 193, 8)
 
-                    '255, 174, 23
-                    rFrom2 = 255
-                    gFrom2 = 174
-                    bFrom2 = 23
-
-                    '255, 193, 8
-                    rTo2 = 255
-                    gTo2 = 193
-                    bTo2 = 8
                 Case "81,100,100,81,5,100"
-                    rFrom = 251
-                    gFrom = 255
-                    bFrom = 242
 
-                    rTo = 187
-                    gTo = 255
-                    bTo = 100
+                    fromColor = RGB(251, 255, 242)
+                    toColor = RGB(187, 255, 100)
 
-                    rFrom2 = 187
-                    gFrom2 = 255
-                    bFrom2 = 60
+                    fromColor2 = RGB(187, 255, 60)
+                    toColor2 = RGB(166, 255, 0)
 
-                    rTo2 = 166
-                    gTo2 = 255
-                    bTo2 = 0
                 Case "45,97,100,45,5,100"
-                    rFrom = 255
-                    gFrom = 242
-                    bFrom = 217
-
+                    fromColor = RGB(255, 242, 217)
+                    toColor = RGB(255, 221, 157)
                     '255, 202, 102
-                    rTo = 255
-                    gTo = 221
-                    bTo = 157
 
-                    '255, 174, 23
-                    rFrom2 = 255
-                    gFrom2 = 193
-                    bFrom2 = 79
-
-                    rTo2 = 255
-                    gTo2 = 193
-                    bTo2 = 8
+                    fromColor2 = RGB(255, 193, 79)
+                    toColor2 = RGB(255, 193, 8)
                 Case "273,97,100,273,5,100"
                     '144, 8, 255  
                     '249, 242, 255
-                    rFrom = 249
-                    gFrom = 242
-                    bFrom = 255
+                    fromColor = RGB(249, 242, 255)
+                    toColor = RGB(206, 147, 255)
 
-                    rTo = 206
-                    gTo = 147
-                    bTo = 255
+                    fromColor2 = RGB(174, 74, 255)
+                    toColor2 = RGB(144, 8, 255)
 
-                    rFrom2 = 174
-                    gFrom2 = 74
-                    bFrom2 = 255
-
-                    rTo2 = 144
-                    gTo2 = 8
-                    bTo2 = 255
                 Case "198,97,100,198,5,100"
                     '242, 252, 255
                     '8, 181, 255
-                    rFrom = 242
-                    gFrom = 252
-                    bFrom = 255
+                    fromColor = RGB(242, 252, 255)
+                    toColor = RGB(149, 223, 255)
 
-                    rTo = 149
-                    gTo = 223
-                    bTo = 255
+                    fromColor2 = RGB(85, 204, 255)
+                    toColor2 = RGB(8, 181, 255)
 
-                    rFrom2 = 85
-                    gFrom2 = 204
-                    bFrom2 = 255
-
-                    rTo2 = 8
-                    gTo2 = 181
-                    bTo2 = 255
                 Case "356,97,100,356,5,100"
                     '255, 242, 243
                     '255, 8, 24
-                    rFrom = 255
-                    gFrom = 242
-                    bFrom = 243
+                    fromColor = RGB(255, 242, 243)
+                    toColor = RGB(255, 140, 149)
 
-                    rTo = 255
-                    gTo = 140
-                    bTo = 149
-
-                    rFrom2 = 255
-                    gFrom2 = 74
-                    bFrom2 = 88
-
-                    rTo2 = 255
-                    gTo2 = 8
-                    bTo2 = 24
+                    fromColor2 = RGB(255, 74, 88)
+                    toColor2 = RGB(255, 8, 24)
                 Case Else
                     'TODO: Convert HSV to RGB
-                    rFrom = CInt(Split(strColor, ",")(0))
-                    gFrom = CInt(Split(strColor, ",")(1))
-                    bFrom = CInt(Split(strColor, ",")(2))
+                    Throw New NotImplementedException("Convert HSV to RGB")
+                    'rFrom = CInt(Split(strColor, ",")(0))
+                    'gFrom = CInt(Split(strColor, ",")(1))
+                    'bFrom = CInt(Split(strColor, ",")(2))
 
-                    rTo = CInt(Split(strColor, ",")(3))
-                    gTo = CInt(Split(strColor, ",")(4))
-                    bTo = CInt(Split(strColor, ",")(5))
+                    'rTo = CInt(Split(strColor, ",")(3))
+                    'gTo = CInt(Split(strColor, ",")(4))
+                    'bTo = CInt(Split(strColor, ",")(5))
 
-                    rFrom2 = CInt(Split(strColor, ",")(0))
-                    gFrom2 = CInt(Split(strColor, ",")(1))
-                    bFrom2 = CInt(Split(strColor, ",")(2))
+                    'rFrom2 = CInt(Split(strColor, ",")(0))
+                    'gFrom2 = CInt(Split(strColor, ",")(1))
+                    'bFrom2 = CInt(Split(strColor, ",")(2))
 
-                    rTo2 = CInt(Split(strColor, ",")(3))
-                    gTo2 = CInt(Split(strColor, ",")(4))
-                    bTo2 = CInt(Split(strColor, ",")(5))
+                    'rTo2 = CInt(Split(strColor, ",")(3))
+                    'gTo2 = CInt(Split(strColor, ",")(4))
+                    'bTo2 = CInt(Split(strColor, ",")(5))
             End Select
 
             Dim total As Double = 0.0
             Dim sqrtotal As Double = 0.0
             Dim stdDev As Double
             Dim count As Integer = 0
-            Dim nc, nr As Integer
-            Dim nodata As Double
-            nc = pRaster.Header.NumberCols - 1
-            nr = pRaster.Header.NumberRows - 1
-            nodata = pRaster.Header.NodataValue
-            Dim rowvals() As Single
-            ReDim rowvals(nc)
+            Dim nc As Integer = pRaster.Header.NumberCols - 1
+            Dim nr As Integer = pRaster.Header.NumberRows - 1
+            Dim nodata As Double = pRaster.Header.NodataValue
+            Dim rowvals(nc) As Single
+
             For row As Integer = 0 To nr
                 pRaster.GetRow(row, rowvals(0))
                 For col As Integer = 0 To nc
@@ -240,42 +159,47 @@ Module Rasters
             Next
             stdDev = Math.Sqrt((sqrtotal / count) - (total / count) * (total / count))
 
-            Const stdMult As Integer = 2
-
             'Dim percVal As Double = (stdDev * stdMult) / pRaster.Maximum
             'Dim rstep As Double = (stdMult) * (rTo - rFrom) * percVal
             'Dim gstep As Double = (stdMult) * (gTo - gFrom) * percVal
             'Dim bstep As Double = (stdMult) * (bTo - bFrom) * percVal
 
+            Const stdMult As Integer = 2
+            Dim firstBoundary As Double = pRaster.Minimum + stdMult * stdDev
+            Dim secondBoundary As Double = pRaster.Maximum - stdMult * stdDev
+
             Dim cs As New GridColorScheme
             Dim csbrk As New GridColorBreak
             csbrk.LowValue = pRaster.Minimum
-            csbrk.HighValue = pRaster.Minimum + stdMult * stdDev
+            csbrk.HighValue = firstBoundary
             csbrk.ColoringType = ColoringType.Gradient
-            csbrk.LowColor = Convert.ToUInt32(RGB(CInt(rFrom), CInt(gFrom), CInt(bFrom)))
-            csbrk.HighColor = Convert.ToUInt32(RGB(CInt(rTo), CInt(gTo), CInt(bTo)))
+            csbrk.LowColor = fromColor
+            csbrk.HighColor = toColor
             csbrk.Caption = String.Format("{0} - {1}", csbrk.LowValue, csbrk.HighValue)
             cs.InsertBreak(csbrk)
             csbrk = New GridColorBreak
-            csbrk.LowValue = pRaster.Minimum + stdMult * stdDev
-            csbrk.HighValue = pRaster.Maximum - stdMult * stdDev
+            csbrk.LowValue = firstBoundary
+            csbrk.HighValue = secondBoundary
             csbrk.ColoringType = ColoringType.Gradient
-            csbrk.LowColor = Convert.ToUInt32(RGB(CInt(rTo), CInt(gTo), CInt(bTo)))
-            csbrk.HighColor = Convert.ToUInt32(RGB(CInt(rFrom2), CInt(gFrom2), CInt(bFrom2)))
+            csbrk.LowColor = toColor
+            csbrk.HighColor = fromColor2
             csbrk.Caption = String.Format("{0} - {1}", csbrk.LowValue, csbrk.HighValue)
             cs.InsertBreak(csbrk)
             csbrk = New GridColorBreak
-            csbrk.LowValue = pRaster.Maximum - stdMult * stdDev
+            csbrk.LowValue = secondBoundary
             csbrk.HighValue = pRaster.Maximum
             csbrk.ColoringType = ColoringType.Gradient
-            csbrk.LowColor = Convert.ToUInt32(RGB(CInt(rFrom2), CInt(gFrom2), CInt(bFrom2)))
-            csbrk.HighColor = Convert.ToUInt32(RGB(CInt(rTo2), CInt(gTo2), CInt(bTo2)))
+            csbrk.LowColor = fromColor2
+            csbrk.HighColor = toColor2
             csbrk.Caption = String.Format("{0} - {1}", csbrk.LowValue, csbrk.HighValue)
             cs.InsertBreak(csbrk)
             Return cs
+
         Catch ex As Exception
             HandleError(ex)
+            Return Nothing
         End Try
+
     End Function
 
 End Module
