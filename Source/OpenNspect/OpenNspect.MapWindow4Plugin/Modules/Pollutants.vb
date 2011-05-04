066706b3d0409d74ec7c037263ad6cc665033af1
mudnug_cp <SND\mudnug_cp>
2011-05-04 02:30:46 +0000

    Simplified Code

--- Source/OpenNspect/OpenNspect.MapWindow4Plugin/Modules/Pollutants.vb
+++ Source/OpenNspect/OpenNspect.MapWindow4Plugin/Modules/Pollutants.vb
@@ -92,7 +92,7 @@ Module Pollutants
                         strType = "SELECT LCCLASS.Value, LCCLASS.Name, COEFFICIENT." & strField & " As CoeffType, COEFFICIENT.CoeffID, COEFFICIENT.LCCLASSID " & "FROM LCCLASS LEFT OUTER JOIN COEFFICIENT " & "ON LCCLASS.LCCLASSID = COEFFICIENT.LCCLASSID " & "WHERE COEFFICIENT.COEFFSETID = " & dataPoll("CoeffSetID") & " ORDER BY LCCLASS.VALUE"
                         Using cmdType As New DataHelper(strType)
                             Dim command As OleDbCommand = cmdType.GetCommand()
-                            strConStatement = ConstructPickStatment(command, g_LandCoverRaster)
+                            strConStatement = ConstructPickStatmentUsingLandClass(command, g_LandCoverRaster, "CoeffType")
                             _PollutantCoeffMetadata = ConstructMetaData(command, (Pollutant.strCoeff), g_booLocalEffects)
                         End Using
                     End Using
@@ -149,100 +149,6 @@ Module Pollutants
 
         ConstructMetaData = strHeader & g_strLandCoverParameters & strConstructMetaData & strLandClassCoeff
 
-    End Function
-
-    Private Function ConstructPickStatment(ByRef cmdType As OleDbCommand, ByRef pLCRaster As Grid) As String
-        'Creates the initial pick statement using the name of the the LandCass [CCAP, for example]
-        'and the Land Class Raster.  Returns a string
-
-        ConstructPickStatment = ""
-        Try
-            Dim FieldIndex As Short
-            Dim booValueFound As Boolean
-            Dim i As Short
-            Dim maxVal As Integer = pLCRaster.Maximum
-
-            'TODO: it looks like some of this code is copied, refactor it.
-            Dim tablepath = GetRasterTablePath(pLCRaster)
-            Dim TableExist As Boolean = File.Exists(tablepath)
-            Dim strpick As String = ""
-
-            Dim mwTable As New Table
-            If Not TableExist Then
-                MsgBox("No MapWindow-readable raster table was found. To create one using ArcMap 9.3+, add the raster to the default project, right click on its layer and select Open Attribute Table. Now click on the options button in the lower right and select Export. In the export path, navigate to the directory of the grid folder and give the export the name of the raster folder with the .dbf extension. i.e. if you are exporting a raster attribute table from a raster named landcover, export landcover.dbf into the same level directory as the folder.", MsgBoxStyle.Exclamation, "Raster Attribute Table Not Found")
-
-                Return ""
-            Else
-                mwTable.Open(tablepath)
-
-                FieldIndex = GetFieldIndex(mwTable)
-
-                Dim rowidx As Integer = 0
-                Dim dataType As OleDbDataReader
-                For i = 1 To maxVal
-
-                    If i = 1 Then
-                        If (mwTable.CellValue(FieldIndex, rowidx) = i) Then
-                            dataType = cmdType.ExecuteReader
-                            booValueFound = False
-                            While dataType.Read()
-                                If mwTable.CellValue(FieldIndex, rowidx) = dataType("Value") Then
-                                    booValueFound = True
-                                    strpick = CStr(dataType("CoeffType"))
-                                    rowidx = rowidx + 1
-                                    Exit While
-                                Else
-                                    booValueFound = False
-                                End If
-                            End While
-                            If booValueFound = False Then
-                                MsgBox("Error: Your OpenNSPECT Land Class Table is missing values found in your landcover GRID dataset.")
-                                ConstructPickStatment = Nothing
-                                dataType.Close()
-                                mwTable.Close()
-                                Exit Function
-                            End If
-                            dataType.Close()
-                        Else
-                            strpick = "0"
-                        End If
-                    Else
-                        If (mwTable.CellValue(FieldIndex, rowidx) = i) Then 'And (pRow.Value(FieldIndex) = rsLandClass!Value) Then
-                            dataType = cmdType.ExecuteReader
-
-                            booValueFound = False
-                            While dataType.Read()
-                                If mwTable.CellValue(FieldIndex, rowidx) = dataType("Value") Then
-                                    booValueFound = True
-                                    strpick = String.Format("{0}, {1}", strpick, CStr(dataType("CoeffType")))
-                                    rowidx = rowidx + 1
-                                    Exit While
-                                Else
-                                    booValueFound = False
-                                End If
-                            End While
-                            If booValueFound = False Then
-                                MsgBox("Error: Your OpenNSPECT Land Class Table is missing values found in your landcover GRID dataset.")
-                                ConstructPickStatment = Nothing
-                                dataType.Close()
-                                mwTable.Close()
-                                Exit Function
-                            End If
-                            dataType.Close()
-
-                        Else
-                            strpick = strpick & ", 0"
-                        End If
-                    End If
-                Next
-
-                mwTable.Close()
-            End If
-            Return strpick
-
-        Catch ex As Exception
-            HandleError(ex)
-        End Try
     End Function
 
     Private Sub CalcMassOfPhosperous(ByRef strConStatement As String, ByRef pMassVolumeRaster As Grid)
