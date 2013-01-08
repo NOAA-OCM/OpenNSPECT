d01fecb551fc0d5b510f30854316a9cfaa6fb115
DEslinger_cp <SND\DEslinger_cp>
2013-01-08 18:10:13 +0000

    Added a check of NODATA value when converting the soil shapefile to the two needed rasters.  The program now checks and will change the NODATA value if the default one (taken from the DEM) would be a valid hydrologic data value.

--- Source/OpenNspect/OpenNspect.MapWindow4Plugin/Forms/Additional Dialogs/SoilsSetupForm.vb
+++ Source/OpenNspect/OpenNspect.MapWindow4Plugin/Forms/Additional Dialogs/SoilsSetupForm.vb
@@ -297,6 +297,10 @@ Friend Class SoilsSetupForm
                     Dim headK As New GridHeader
                     head.CopyFrom(dem.Header)
                     headK.CopyFrom(dem.Header)
+                    If dem.Header.NodataValue >= 1.0 Then 'DLE 1/8/2013 Change NODATA value to somethign that is not a valid value
+                        head.NodataValue = -32768.0
+                        headK.NodataValue = -32768.0
+                    End If
                     dem.Close()
 
                     Dim soilsshp As New Shapefile
