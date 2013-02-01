b1fa3d4d2d6a8511f194611e314e7edbd299bd70
Dave Eslinger <dave.eslinger@noaa.gov>
2013-02-01 15:40:13 +0000

    Minor tweaks to fix some comment typos in SoilsSetupForm.vb, change the reported version number to automatically generate private and build values (AssemblyInfo.vb) and to specify that this is built for a 32-bit application (OpenNspect.MapWindow4Plugin.vbproj).

--- Source/OpenNspect/OpenNspect.MapWindow4Plugin/My Project/AssemblyInfo.vb
+++ Source/OpenNspect/OpenNspect.MapWindow4Plugin/My Project/AssemblyInfo.vb
@@ -29,5 +29,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.1.1.0")> 
+'
+'<Assembly: AssemblyVersion("1.1.1.0")> 
+<Assembly: AssemblyVersion("1.1.*")> 
 <Assembly: AssemblyFileVersion("1.1.1.0")> 
